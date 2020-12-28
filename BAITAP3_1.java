3379dc0c0d7a4955aac4c7cb6b3921c8e9dc8137
tuanladonga <76670448+tuanladonga@users.noreply.github.com>
2020-12-28 08:33:40 +0700

    Add files via upload

---
+++ BAITAP3_1.java
@@ -0,0 +1,18 @@
+package chuong03_exercise;
+
+import java.util.Scanner;
+
+public class BAITAP3_1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Nhap a: ");
+        int a = s.nextInt();
+        System.out.println("Nhap b: ");
+        int b = s.nextInt();
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " +(a * b));
+        System.out.println("a / b = " + (a / b));
+
+    }
+}
